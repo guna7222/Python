0a574ad3c182952ce90bb7db83bb53314c8f80f5
Guna Sekhar Ragavaraju <47294637+guna7222@users.noreply.github.com>
2024-11-26 17:22:41 +0530

    Create intro.py

---
+++ basics/intro.py
@@ -0,0 +1,36 @@
+"""
+Python is a popular programming language and it was created by Guido Van Rossum
+in the year of 1991
+- python can be used for software development
+- python can be used for web development
+- python can be used for system scripting
+"""
+
+import sys
+
+# print() function is used to print something in the console
+print("Hello, World!")
+
+# .py is the extension for the python
+
+# how to check python version
+# python --version
+
+var = sys.version
+print(var)
+
+# Indentation
+"""
+Indentation refers to the white spaces at the beginning of the code
+where in other programming languages indention is used for code readability
+but in python indentation indicates a block of code 
+"""
+
+if(5 > 2):
+    print("True " + "True")
+
+# comments
+# in python comments are used to make code more readable
+"""
+multiline comments example
+"""
