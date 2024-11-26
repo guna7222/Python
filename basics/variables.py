585bf84e4193c2957626c43b3968fe37b30ae6b0
Guna Sekhar Ragavaraju <47294637+guna7222@users.noreply.github.com>
2024-11-26 17:23:18 +0530

    Create variables.py

---
+++ basics/variables.py
@@ -0,0 +1,34 @@
+# Variables are containers for storing data values
+# In Python variables are created the moment when you assign a value to it
+# Python is a dynamic programming language where you have to explicitly specify the data types of the variable
+
+x = 5
+name = "GS"
+
+print(x, name)
+
+# Casting
+# if you want to specify the data type of the variable explicitly
+x = int(5)
+name = str("guna")
+decimal_number = float(5.4)
+
+print(x, name, decimal_number)
+
+# type()
+# how to check type of variable
+# in order to check data type of variable in python we can use type() function
+print(type(x))
+
+# Variables are case-sensitive
+x = 5
+X = 5
+# both are not same
+
+#naming convention
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain Alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
