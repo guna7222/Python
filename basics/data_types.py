983852a03ba45164336f5be2896ccb0fee66fe33
Guna Sekhar Ragavaraju <47294637+guna7222@users.noreply.github.com>
2024-11-27 07:14:06 +0530

    Create data_types.py

---
+++ basics/data_types.py
@@ -0,0 +1,52 @@
+"""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+text_type = 'str'
+
+int_type = 5
+float_type = 4.5
+complex_type = 1j
+
+list_type = ['Python', 'java', 'go']
+set_type = ('ec2', 'iam', 's3' )
+range_type = range(5)
+
+mapping_type = {'key1':'value1', 'key2':'value2'}
+
+set_type = {"set1", "set2", "set3"}
+
+boolean_type = True
+
+None_type = None
+
+print(list_type)
+
+print(mapping_type)
+
+# explicitly assigning data type
+name = str("guna")
+
+age = int(26)
+height = float(5.11)
+complex_type = complex(1j)
+
+print("complex " , complex_type)
+
+fruits = list(("apple", "banana"))
+tuple_type = tuple(("names", "java"))
+range_type = range(5)
+
+mapping = dict(name="guna", age= 34)
+set_type1 = set(("set1", "set2"))
+
+boolean = bool(True)
+
+
